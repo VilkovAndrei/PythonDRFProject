0ce99461bcce393da006f5beeea2a1680c1f8e46
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-06-04 14:47:11 +0300

    edit fixtures 2 data.json

--- users/views.py
+++ users/views.py
@@ -4,12 +4,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 
 from users.models import User, Payment
-from users.permissions import UserPermissionsDestroy
-from users.serializers import UserSerializer, PaymentSerializer, UserDetailSerializer
-from rest_framework import viewsets
-
-from users.models import User
-from users.serializers import UserSerializer, MyTokenObtainPairSerializer
+from users.permissions import UserPermissionsDestroy, IsOwner
+from users.serializers import (UserSerializer, PaymentSerializer, UserDetailSerializer, UserLimitedSerializer,
+                               MyTokenObtainPairSerializer)
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -33,15 +30,22 @@ class UserCreateView(generics.CreateAPIView):
 
 
 class UserRetrieveView(generics.RetrieveAPIView):
-    serializer_class = UserDetailSerializer
+    # serializer_class = UserDetailSerializer
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.user.id == self.kwargs["pk"] or self.request.user.is_superuser:
+            self.serializer_class = UserDetailSerializer
+        else:
+            self.serializer_class = UserLimitedSerializer
+        return self.serializer_class
 
 
 class UserUpdateView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner]
 
 
 class UserDestroyView(generics.DestroyAPIView):
@@ -60,21 +64,21 @@ class PaymentListView(generics.ListAPIView):
     filter_backends = [OrderingFilter, DjangoFilterBackend]
     filterset_fields = ['lesson', 'course', 'payment_method']
     ordering_fields = ['date']
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner]
 
 
 class PaymentRetrieveView(generics.RetrieveAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner]
 
 
 class PaymentUpdateView(generics.UpdateAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner]
 
 
 class PaymentDestroyView(generics.DestroyAPIView):
     queryset = Payment.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner]
